6d452c4d5aafcb91f5c48186a1204157696f84e4
Sam-gonz29 <109189148+Sam-gonz29@users.noreply.github.com>
2022-07-13 05:31:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in makwiz_salary_report/__init__.py
@@ -10,17 +9,18 @@ with open('makwiz_salary_report/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+with open('requirements.txt') as f:
+        install_requires = f.read().strip().split('\n')
 
 setup(
-	name='makwiz_salary_report',
-	version=version,
-	description='Custom Salary Report',
-	author='MAKWIZ TECHNOLOGIES',
-	author_email='kmodi@makwiz.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+        name='makwiz_salary_report',
+        version=version,
+        description='Custom Salary Report',
+        author='MAKWIZ TECHNOLOGIES',
+        author_email='kmodi@makwiz.com',
+        packages=find_packages(),
+        zip_safe=False,
+        include_package_data=True,
+        install_requires=[str(ir.req) for ir in requirements],
+        dependency_linksy actualiceinstall_requires=install_requires
 )
